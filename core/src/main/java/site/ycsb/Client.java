d13ead6a0e1659d8e0e90abac47607df6c661671
Carl Camilleri <carlcamilleri@gmail.com>
2022-06-29 16:13:11 +0200

    pooled connections

--- core/src/main/java/site/ycsb/Client.java
+++ core/src/main/java/site/ycsb/Client.java
@@ -348,6 +348,8 @@ public final class Client {
         executor.execute(t);
       }
 
+
+
       if (maxExecutionTime > 0) {
         terminator = new TerminatorThread(maxExecutionTime, threads.keySet(), workload);
         terminator.start();
@@ -357,12 +359,15 @@ public final class Client {
 
       for (Map.Entry<Thread, ClientThread> entry : threads.entrySet()) {
         try {
+          executor.wait();
           entry.getKey().join();
           opsDone += entry.getValue().getOpsDone();
         } catch (InterruptedException ignored) {
           // ignored
         }
       }
+
+
 
       en = System.currentTimeMillis();
     }
