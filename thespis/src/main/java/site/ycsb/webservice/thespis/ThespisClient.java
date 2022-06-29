71792cb04676ddd48625c3c404e1ae1d9bad0db2
Carl Camilleri <carlcamilleri@gmail.com>
2022-06-29 15:36:53 +0200

    pooled connections

--- thespis/src/main/java/site/ycsb/webservice/thespis/ThespisClient.java
+++ thespis/src/main/java/site/ycsb/webservice/thespis/ThespisClient.java
@@ -23,7 +23,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.*;
-import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.zip.GZIPInputStream;
 
